a6bdd0a44c7de5c8a856ba2894393079a942fedd
Adry Parcerisa <aparce@gmail.com>
2020-01-10 12:51:41 -0300

    cantidad de lineas flexible y captura de tamano de lienzo por contexto de documento

--- dibujo.js
+++ dibujo.js
@@ -4,6 +4,8 @@ boton.addEventListener ("click",dibujoPorCick);
 
 var d=document.getElementById("dibujito");
 var lienzo = d.getContext ("2d");
+var ancho = d.width;
+
 
 function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -22,16 +24,19 @@ function dibujoPorCick()
   var l=0;
   var yi,xf;
   var colorcito = "#FAA";
+  var espacio = 0;
+
+  espacio = ancho/lineas;
 
   for (l= 0; l < lineas; l++)
   {
-    yi = 10 *l;
-    xf = 10 * (l+1);
-    dibujarLinea (colorcito, 0 , yi, xf, 300 );
+    yi = espacio *l;
+    xf = espacio * (l+1);
+    dibujarLinea (colorcito, 0 , yi, xf, ancho );
     console.log ("linea " + l);
   }
 
-  dibujarLinea (colorcito, 1,1,1,299);
-  dibujarLinea (colorcito, 1,299,299,299);
+  dibujarLinea (colorcito, 1,1,1,ancho -1);
+  dibujarLinea (colorcito, 1,ancho -1,ancho -1,ancho -1);
 
 }
